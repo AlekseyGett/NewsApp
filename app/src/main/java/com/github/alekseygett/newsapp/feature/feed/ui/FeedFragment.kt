c8636bf5649690763efa004dbdbc9b5e398da4e5
Aleksey Get <aleksey.gett@gmail.com>
2021-10-24 17:19:15 +0800

    Create feed layout
    * Create FeedFragment layout
    * Create ArticleAdapter
    * Create item layout

--- app/src/main/java/com/github/alekseygett/newsapp/feature/feed/ui/FeedFragment.kt
+++ app/src/main/java/com/github/alekseygett/newsapp/feature/feed/ui/FeedFragment.kt
@@ -1,6 +1,11 @@
 package com.github.alekseygett.newsapp.feature.feed.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.alekseygett.newsapp.databinding.FragmentFeedBinding
 
 class FeedFragment : Fragment() {
 
@@ -8,4 +13,31 @@ class FeedFragment : Fragment() {
         fun newInstance() = FeedFragment()
     }
 
+    private var _binding: FragmentFeedBinding? = null
+
+    // Only valid between onCreateView and onDestroyView
+    private val binding
+        get() = _binding!!
+
+    private val articlesAdapter: ArticlesAdapter by lazy {
+        ArticlesAdapter(
+            articles = emptyList(),
+            onBookmarkButtonClick = { },
+            onItemClick = { }
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFeedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView.adapter = articlesAdapter
+    }
 }
